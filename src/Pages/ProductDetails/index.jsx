7e374fe740f578c19db6e9d525de7bd075027ea7
ducanhgit2004 <nducanh485@gmail.com>
2026-05-13 00:34:56 +0700

    quantity

--- src/Pages/ProductDetails/index.jsx
+++ src/Pages/ProductDetails/index.jsx
@@ -1,11 +1,17 @@
 import Breadcrumbs from "@mui/material/Breadcrumbs";
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from "react-router-dom";
 import ProductZoom  from "../../components/ProductZoom"
 import Rating from '@mui/material/Rating'
+import Button from "@mui/material/Button";
+import QtyBox from "../../components/QtyBox";
+import { MdOutlineShoppingCart } from "react-icons/md";
 
 
 const ProductDetails = () => {
+
+    const[productActionIndex, setProductActionIndex] = useState(null);
+
   return (
     <>
       <div className="py-5">
@@ -66,13 +72,14 @@ const ProductDetails = () => {
 
               </div>
               <div className="flex items-center gap-4 mt-4">
-            <span className="oldPrice line-through text-gray-500 text-[18px] font-[500]">$58.00</span>
-            <span className="price text-[#ff5252] font-[600] text-[18px]">$30.00</span>
+            <span className="oldPrice line-through text-gray-500 text-[20px] font-[500]">$58.00</span>
+            <span className="price text-[#ff5252] font-[600] text-[20px]">$30.00</span>
+
+            <span className="text-[14px]">Available In Stock : <span className="text-green-600
+            text-[14px] font-bold"> 147 Items</span></span>
           </div>
 
-          <br/>
-
-            <p>
+            <p className="mt-3 pr-10 mb-5">
               - Quần giả váy xếp ly đẹp, tạo hiệu ứng chuyển động nhẹ nhàng và tôn dáng.<br/>
 
               - Chất liệu mềm mại, thoáng nhẹ, mang lại cảm giác dễ chịu khi mặc cả ngày.<br/>
@@ -85,6 +92,33 @@ const ProductDetails = () => {
 
               S: 31cm - M: 32cm - L: 33cm - XL: 34cm <br/>
             </p>
+
+            <div className="flex items-center gap-3">
+                <span className="text-[16px]">Size:</span>
+                <div className="flex items-center gap-1 actions">
+                    <Button className={`${productActionIndex === 0 ? '!bg-[#ff5252] !text-white':''}`} 
+                    onClick={()=>setProductActionIndex(0)}>S</Button>
+                    <Button className={`${productActionIndex === 1 ? '!bg-[#ff5252] !text-white':''}`} 
+                    onClick={()=>setProductActionIndex(1)}>M</Button>
+                    <Button className={`${productActionIndex === 2 ? '!bg-[#ff5252] !text-white':''}`} 
+                    onClick={()=>setProductActionIndex(2)}>L</Button>
+                    <Button className={`${productActionIndex === 3 ? '!bg-[#ff5252] !text-white':''}`} 
+                    onClick={()=>setProductActionIndex(3)}>XL</Button>
+                </div>
+            </div>
+
+
+          <p className="text-[14px] mt-4 mb-2">FreeShipping (Est. Delivery Time 2-3 Days)</p>
+            <div className="flex items-center  gap-4">
+              <div className="qtyBoxWrapper w-[70px]">
+                <QtyBox/>
+              </div>
+            
+
+            <Button className="btn-org flex gap-2">
+              <MdOutlineShoppingCart className="text-[22px]"/> 
+            Add to Cart </Button>
+            </div>
 
           </div>
         </div>
---
+++ src/components/QtyBox/index.jsx
@@ -0,0 +1,42 @@
+import Button from '@mui/material/Button'
+import React, { useState } from 'react'
+import { FaAngleUp } from "react-icons/fa6";
+import { FaAngleDown } from "react-icons/fa6";
+
+
+const QtyBox = () => {
+    
+    const [qtyVal, setQtyVal] = useState(1);
+
+    const plusQty=()=>{
+        setQtyVal(qtyVal+1)
+    }
+
+    const minusQty=()=>{
+        if(qtyVal===1){
+            setQtyVal(1)
+        }else{
+         setQtyVal(qtyVal-1)  
+        }
+       
+    }
+
+
+  return (
+    <div className='qtyBox flex items-center relative'>
+      <input type='number' className='w-full h-[40px] p-2 pl-5 text-[15px] 
+      focus:outline-none border border-[rgba(0,0,0,0.2)] rounded-md' value={qtyVal} />
+
+    <div className='flex items-center flex-col justify-between h-[40px] absolute 
+    top-0 right-0 z-50'>
+        <Button className='!min-w-[30px] !w-[30px] !h-[20px] !text-[#000] !rounded-none'
+        onClick={plusQty}><FaAngleUp/></Button>
+        <Button className='!min-w-[30px] !w-[30px] !h-[20px] !text-[#000] !rounded-none'
+        onClick={minusQty}><FaAngleDown/></Button>
+    </div>
+
+    </div>
+  )
+}
+
+export default QtyBox
